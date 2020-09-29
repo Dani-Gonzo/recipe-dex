46389c2441c218d9219107a1ce04b09cda1ced0f
Danielle L <d.gonso@hotmail.com>
2020-09-28 19:51:22 -0700

    add grocery list checkbox

--- screens/groceryList.js
+++ screens/groceryList.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, ScrollView} from 'react-native';
 import {globalStyles} from '../styles/global';
 import {CustomText} from '../customs/customText';
 import Card from '../templates/card';
+import CheckBox from 'react-native-check-box'
 
 export default function GroceryList() {
+    const [isChecked, setIsChecked] = useState(true);
+
     return (
         <ScrollView style={globalStyles.container}>
             <Card style={styles.groceryCard}>
+                <CheckBox
+                    style={styles.checkBox}
+                    onClick={()=>{
+                        setIsChecked(!isChecked);
+                        // this.setState({
+                        //     isChecked:!this.state.isChecked
+                        // })
+                    }}
+                    isChecked={isChecked}
+                    // leftText={"CheckBox"}
+                    checkBoxColor="lightgray"
+                    checkedCheckBoxColor="lightblue"
+                />
                 <CustomText>Test Item</CustomText>
             </Card>
         </ScrollView>
@@ -22,6 +38,12 @@ const styles = StyleSheet.create({
     },
     groceryCard: {
         // marginHorizontal: 16,
-        marginVertical: 16
+        marginVertical: 16,
+        flexDirection: "row",
+        // justifyContent: "flex-start",
+        alignItems: "center"
+    },
+    checkBox: {
+        paddingRight: 15
     }
 });
